604806825f8f60376d97a81e652796560473dd8d
RupanGuvi <rupanguvi@gmail.com>
2024-11-05 19:04:23 +0530

    navbar completed

---
+++ src/Pages/CreateBlog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateBlog = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">create Blog</h1>
+    </div>
+  );
+};
+
+export default CreateBlog;
